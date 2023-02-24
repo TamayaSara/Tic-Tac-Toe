49dfb77500c35b41f90c12c7a76de846e57865b5
Tamaya Sara <95479134+TamayaSara@users.noreply.github.com>
2023-02-24 16:10:15 -0500

    Push all the updated files to the repo

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `That's a tie!!!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 
 init();
