e607aa6374c3c32a2633035a8bbb40cf9d400ba3
Tamaya Sara <95479134+TamayaSara@users.noreply.github.com>
2023-02-24 14:19:28 -0500

    added the constants to the main.java file

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.') 
+/*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
